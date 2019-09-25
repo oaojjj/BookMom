3aae36ec9d7f8d158255fd3e39d20507ad548c49
Jae-Woo Joo <wnwodn4293@naver.con>
2019-09-25 16:30:09 +0900

    modify

--- 주재우 (PHP)/checkid.php
+++ 주재우 (PHP)/checkid.php
@@ -1,16 +1,16 @@
 <?php
 include ('dbcon.php');
-$name = $_GET["name"];
-$sql="select * from user where name='$name'";
+$id = $_GET["id"];
+$sql="select * from user where id='$id'";
 $result=mysqli_query($con,$sql);
 $count=mysqli_num_rows($result);
 if($count==0){
   ?>
-  	<div style='font-family:"malgun gothic"';><?php echo $name; ?>는 사용가능한 아이디입니다.</div>
+  	<div style='font-family:"malgun gothic"';><?php echo $id; ?>는 사용가능한 아이디입니다.</div>
   <?php
   	}else{
   ?>
-  <div style='font-family:"malgun gothic"; color:red;'><?php echo $name; ?>는 중복된아이디입니다.<div>
+  <div style='font-family:"malgun gothic"; color:red;'><?php echo $id; ?>는 중복된아이디입니다.<div>
   <?php
 }
   ?>
--- 주재우 (PHP)/insert.php
+++ 주재우 (PHP)/insert.php
@@ -8,12 +8,12 @@
 
     if( ($_SERVER['REQUEST_METHOD'] == 'POST') && isset($_POST['submit']))
     {
-
+        $id=$_POST['id'];
         $name=$_POST['name'];
         $password=md5($_POST['password']);
         if(!isset($errMSG))
         {
-          $sql="insert into user(name,password) values('$name','$password')";
+          $sql="insert into user(id,name,password) values('$id','$name','$password')";
           if (!mysqli_query($con,$sql))
     {
     die('Error: ' . mysqli_error($con));
@@ -25,9 +25,9 @@
 <html>
 <script>
 function checkid(){
-  var name=document.getElementById("name").value;
-  if(name!==""&&name!=null){
-    url="checkid.php?name="+name;
+  var id=document.getElementById("id").value;
+  if(id!==""&&id!=null){
+    url="checkid.php?id="+id;
     window.open(url,"chkid","width=300,height=100");
   }
   else{alert("아이디를 입력하세요")}
@@ -39,9 +39,9 @@ function checkid(){
         if (isset($errMSG)) echo $errMSG;
         if (isset($successMSG)) echo $successMSG;
         ?>
-
         <form action="insert.php" method="POST">
-            Name: <input type = "text" name = "name" id="name" required /> <input type="button" value="중복검사" onclick="checkid();"><br>
+            id: <input type = "text" name = "id" id="id" required /> <input type="button" value="중복검사" onclick="checkid();"><br>
+            name : <input type = "text" name = "name" id="name" required /><br>
             password: <input type = "password" name = "password" required/>
             <input type = "submit" value="회원가입" name = "submit" /> <br>
             <input type="button" value="로그인창" onclick="location.href='./login.php'">
